82c41b2e0b1eaf495adcb8f5dc0d69aaa083ccbc
Lordimass <sam@tchrin.net>
2024-07-23 10:55:11 +0100

    Testing new redirect fix

--- redirect.js
+++ redirect.js
@@ -1,3 +1,12 @@
 const currentPath = window.location.pathname;
 const newURL = `https://reformationfundraising.com${currentPath}`;
+console.log(`redirecting to ${newURL}`)
 window.location.replace(newURL);
+
+const body = document.getElementById("redirect-info")
+const link = document.createElement("a")
+link.innerText = "here"
+link.setAttribute("href", newURL)
+
+body.innerText = `Redirecting to ${newURL}, if you are not redirected, click `
+body.appendChild(link)
